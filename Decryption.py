1c6560de1de0f4dc9cdbe5599ac98df1e01eb41e
ANANDHU <154292503+9northwind@users.noreply.github.com>
2024-01-21 21:57:46 +0530

    Update Decryption.py

--- Decryption.py
+++ Decryption.py
@@ -18,32 +18,26 @@ class Decryption:
                     char_index = ord(char) - ord('A') + 1  # Finding the position of character in alphabet
                     new_char_index = (char_index - self.key) % 26
                     new_char = chr(new_char_index + ord('A') - 1)  # Finding the character at new index
-                    print(new_char)
                     self.original += new_char
                 elif char.islower():
                     char_index = ord(char) - ord('a') + 1
                     new_char_index = (char_index - self.key) % 26
                     new_char = chr(new_char_index + ord('a') - 1)
-                    print(new_char)
                     self.original += new_char
                 elif char in symbols:
                     char_index = symbols.index(char)
                     new_char_index = (char_index - self.key) % 31
                     new_char = symbols[new_char_index]
-                    print(new_char)
                     self.original += new_char
                 elif char.isdigit():
                     char_index = numbers.index(char)
                     new_char_index = (char_index - self.key) % 10
                     new_char = numbers[new_char_index]
-                    print(new_char)
                     self.original += new_char
                 elif char == '`':
                     self.original += ' '
             with open(self.filepath, 'w') as f:
                 f.write(self.original)
-
-            print(self.original)
 
         except FileNotFoundError:
             print("File not found.")
