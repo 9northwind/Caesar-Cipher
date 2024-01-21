abecc4ee1a4e643b4b342687d69a73cf01dc1c1d
ANANDHU <154292503+9northwind@users.noreply.github.com>
2024-01-21 21:52:54 +0530

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -17,30 +17,25 @@ class Encryption:
                     char_index = ord(char) - ord('A') + 1  # Finding the position of character in alphabet
                     new_char_index = (char_index + self.key) % 26
                     new_char = chr(new_char_index + ord('A') - 1)  # Finding the character at new index
-                    print(new_char)
                     self.cipher += new_char
                 elif char.islower():
                     char_index = ord(char) - ord('a') + 1
                     new_char_index = (char_index + self.key) % 26
                     new_char = chr(new_char_index + ord('a') - 1)
-                    print(new_char)
                     self.cipher += new_char
                 elif char in symbols:
                     char_index = symbols.index(char)
                     new_char_index = (char_index + self.key) % 31
                     new_char = symbols[new_char_index]
-                    print(new_char)
                     self.cipher += new_char
                 elif char.isdigit():
                     char_index = numbers.index(char)
                     new_char_index = (char_index + self.key) % 10
                     new_char = numbers[new_char_index]
-                    print(new_char)
                     self.cipher += new_char
                 elif char == ' ':
                     self.cipher += '`'
             with open(self.filepath, 'w') as f:
                 f.write(self.cipher)
-            print(self.cipher)
         except FileNotFoundError:
             print("File not found.")
